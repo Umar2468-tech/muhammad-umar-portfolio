456d3381ee97f2b11df989b143086872873f938b
hamdan <l1f22bscs0160@ucp.edu.pk>
2026-07-14 18:55:45 +0500

    remove trial-gsap-versio

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,5 +1,4 @@
-import { SplitText } from "gsap-trial/SplitText";
-import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";import gsap from "gsap";
 import { smoother } from "../Navbar";
 
 export function initialFX() {
